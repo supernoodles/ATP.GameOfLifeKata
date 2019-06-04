2c1b650b7be71b9acdca19492bbe37eb3d497e90
Steve Cusack <stephen.cusack@mandg.com>
2019-06-04 12:25:07 +0100

    Add project files.

---
+++ ATP.GameOfLifeKata.Tests/GameOfLifeShould.cs
@@ -0,0 +1,42 @@
+﻿
+namespace ATP.GameOfLifeKata.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GameOfLifeShould
+    {
+        [Test]
+        public void ShouldReturnDeadGrid_GivenDeadGrid()
+        {
+            var deadGame = new GameOfLife();
+            var game = new GameOfLife();
+
+            game.Tick();
+
+            game.Should().Be(deadGame);
+        }
+    }
+
+    public class GameOfLife
+    {
+        public void Tick()
+        {
+        }
+
+        protected bool Equals(GameOfLife other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((GameOfLife) obj);
+        }
+
+    }
+}
---
+++ ATP.GameOfLifeKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ATP.GameOfLifeKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATP.GameOfLifeKata.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12d83498-c837-41f1-97a3-298b0f91f5ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
